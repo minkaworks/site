192a7956979ecc78658c7d98fff435964fd703bb
jdqniel <47310042+jdqniel@users.noreply.github.com>
2026-05-17 21:57:02 -0600

    Add Codeberg footer link

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -24,6 +24,14 @@ function GitHubIcon() {
   )
 }
 
+function CodebergIcon() {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 24 24">
+      <path d="M11.999.747A11.974 11.974 0 0 0 0 12.75c0 2.254.635 4.465 1.833 6.376L11.837 6.19c.072-.092.251-.092.323 0l4.178 5.402h-2.992l.065.239h3.113l.882 1.138h-3.674l.103.374h3.86l.777 1.003h-4.358l.135.483h4.593l.695.894h-5.038l.165.589h5.326l.609.785h-5.717l.182.65h6.038l.562.727h-6.397l.183.65h6.717A12.003 12.003 0 0 0 24 12.75 11.977 11.977 0 0 0 11.999.747zm3.654 19.104.182.65h5.326c.173-.204.353-.433.513-.65zm.385 1.377.18.65h3.563c.233-.198.485-.428.712-.65zm.383 1.377.182.648h1.203c.356-.204.685-.412 1.042-.648zz" />
+    </svg>
+  )
+}
+
 export function generateStaticParams() {
   return locales.map((lang) => ({ lang }))
 }
@@ -165,15 +173,26 @@ export default async function Home({ params }: { params: Promise<{ lang: string 
 
       <footer className="siteFooter">
         <div className="siteFooterInner">
-          <a
-            className="siteFooterGithub"
-            href="https://github.com/minkaworks"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Minka Works GitHub"
-          >
-            <GitHubIcon />
-          </a>
+          <div className="siteFooterSocials">
+            <a
+              className="siteFooterSocial"
+              href="https://github.com/minkaworks"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Minka Works GitHub"
+            >
+              <GitHubIcon />
+            </a>
+            <a
+              className="siteFooterSocial"
+              href="https://codeberg.org/minkaworks"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Minka Works Codeberg"
+            >
+              <CodebergIcon />
+            </a>
+          </div>
           <span>
             &copy; {copy.footer.company}
           </span>
